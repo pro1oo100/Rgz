d6d187809b907f18180710a409ed77c2e08cb75b
komarov <user@Users-MacBook-Pro.local>
2026-06-19 18:59:58 +0700

    fix

--- libs/AffineCipher/AffineCipher.cpp
+++ libs/AffineCipher/AffineCipher.cpp
@@ -1,78 +1,74 @@
 #include "../../include/cipher_interface.h"
 
-#include <algorithm>
 #include <cstring>
 #include <random>
 #include <sstream>
 #include <stdexcept>
 #include <vector>
 
-namespace
+static std::pair<uint8_t, uint8_t> parseKey(const char* key)
 {
-    std::pair<uint8_t, uint8_t> parse_key(const char* key)
+    std::string s(key);
+    size_t comma = s.find(',');
+
+    if (comma == std::string::npos)
     {
-        std::string s(key);
-        size_t comma = s.find(',');
-
-        if (comma == std::string::npos)
-        {
-            throw std::runtime_error("ключ Affine Cipher должен быть в формате \"a,b\"");
-        }
-
-        int a = std::stoi(s.substr(0, comma));
-        int b = std::stoi(s.substr(comma + 1));
-
-        if (a <= 0 || a >= 256 || a % 2 == 0)
-        {
-            throw std::runtime_error("a должно быть нечётным числом в диапазоне [1, 255]");
-        }
-
-        if (b < 0 || b >= 256)
-        {
-            throw std::runtime_error("b должно быть в диапазоне [0, 255]");
-        }
-
-        return {uint8_t(a), uint8_t(b)};
+        throw std::runtime_error("ключ Affine Cipher должен быть в формате \"a,b\"");
     }
 
-    uint8_t mod_inverse(uint8_t a)
+    int a = std::stoi(s.substr(0, comma));
+    int b = std::stoi(s.substr(comma + 1));
+
+    if (a <= 0 || a >= 256 || a % 2 == 0)
     {
-        int t = 0;
-        int new_t = 1;
-        int r = 256;
-        int new_r = a;
-
-        while (new_r != 0)
-        {
-            int q = r / new_r;
-            int tmp = t - q * new_t;
-            t = new_t;
-            new_t = tmp;
-
-            tmp = r - q * new_r;
-            r = new_r;
-            new_r = tmp;
-        }
-
-        if (r != 1)
-        {
-            throw std::runtime_error("a не имеет обратного элемента по модулю 256");
-        }
-
-        if (t < 0)
-        {
-            t += 256;
-        }
-
-        return uint8_t(t);
+        throw std::runtime_error("a должно быть нечётным числом в диапазоне [1, 255]");
     }
+
+    if (b < 0 || b >= 256)
+    {
+        throw std::runtime_error("b должно быть в диапазоне [0, 255]");
+    }
+
+    return {uint8_t(a), uint8_t(b)};
+}
+
+static uint8_t modInverse(uint8_t a)
+{
+    int t = 0;
+    int newT = 1;
+    int r = 256;
+    int newR = a;
+
+    while (newR != 0)
+    {
+        int q = r / newR;
+        int tmp = t - q * newT;
+        t = newT;
+        newT = tmp;
+
+        tmp = r - q * newR;
+        r = newR;
+        newR = tmp;
+    }
+
+    if (r != 1)
+    {
+        throw std::runtime_error("a не имеет обратного элемента по модулю 256");
+    }
+
+    if (t < 0)
+    {
+        t += 256;
+    }
+
+    return uint8_t(t);
 }
 
 extern "C"
 {
     CipherResult encrypt(const uint8_t* data, uint64_t size, const char* key)
     {
-        auto [a, b] = parse_key(key);
+        auto [a, b] = parseKey(key);
 
         CipherResult result;
         result.size = size;
@@ -88,8 +84,8 @@ extern "C"
 
     CipherResult decrypt(const uint8_t* data, uint64_t size, const char* key)
     {
-        auto [a, b] = parse_key(key);
-        uint8_t a_inv = mod_inverse(a);
+        auto [a, b] = parseKey(key);
+        uint8_t aInv = modInverse(a);
 
         CipherResult result;
         result.size = size;
@@ -97,7 +93,7 @@ extern "C"
 
         for (uint64_t i = 0; i < size; ++i)
         {
-            result.data[i] = uint8_t((a_inv * (data[i] + 256 - b)) % 256);
+            result.data[i] = uint8_t((aInv * (data[i] + 256 - b)) % 256);
         }
 
         return result;
@@ -115,11 +111,11 @@ extern "C"
             odds.push_back(uint8_t(i));
         }
 
-        std::uniform_int_distribution<size_t> dist_a(0, odds.size() - 1);
-        std::uniform_int_distribution<int> dist_b(0, 255);
+        std::uniform_int_distribution<size_t> distA(0, odds.size() - 1);
+        std::uniform_int_distribution<int> distB(0, 255);
 
-        uint8_t a = odds[dist_a(gen)];
-        uint8_t b = uint8_t(dist_b(gen));
+        uint8_t a = odds[distA(gen)];
+        uint8_t b = uint8_t(distB(gen));
 
         std::ostringstream out;
         out << int(a) << "," << int(b);
