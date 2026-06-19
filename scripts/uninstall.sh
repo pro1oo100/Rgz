9002749f25772a82b8c6e2c591146e93e67fd11a
grisha <birgregory2007@gmail.com>
2026-06-19 18:53:53 +0700

    fix

--- scripts/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# uninstall.sh вЂ” removes cryptum from /usr/local (requires root)
-set -e
-
-rm -f /usr/local/bin/cryptum
-rm -f /usr/local/lib/libatbash.so
-rm -f /usr/local/lib/librc4.so
-ldconfig
-echo "cryptum uninstalled."
